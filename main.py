dd4e1356b63def69343c9ba28de6ab4143abbe21
Ефим Кубышкин <fiska2003@mail.ru>
2022-10-04 15:59:38 +0300

    Rewrote/added some async function

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import Parser
-from time import time
-import asyncio
 
 if __name__ == '__main__':
     N = int(input("Введите колличество фотографий: "))
-    t0 = time()
+    # список токенов и спиок id записываются в файлы, соответственно, "fileWithTokens.txt", "fileWithIds.txt", результат
+    # записывается в файл result_pars.txt
     Parser.async_parse('fileWithTokens.txt', 'fileWithIds.txt', N)
-    print(time() - t0)
-    t1 = time()
-    Parser.parse('fileWithTokens.txt', 'fileWithIds.txt', N)
-    print(time() - t1)
+
